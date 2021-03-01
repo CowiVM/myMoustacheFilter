9041d7efd157e20a7855f9d0feb285cf0f22e783
CowiVM <72988033+CowiVM@users.noreply.github.com>
2021-03-01 14:55:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 function setup() {
-    canvas = createCanvas(300, 300)
+    canvas = createCanvas(350, 300)
     canvas.center()
+    video = createCapture(VIDEO)
+    video.hide(350,300)
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotPoses)
 }
 
 function draw() {
-
+    image(video, 0, 0, 350, 300)
 }
 
 function take_snapshot() {
     save("My Filter Image.png")
+}
+
+function modelLoaded() {
+    console.log("PoseNet Is Initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        console.log("nose_x = " + results[0].pose.nose.x);
+        console.log("nose_y = " + results[0].pose.nose.y);
+    }
 }
